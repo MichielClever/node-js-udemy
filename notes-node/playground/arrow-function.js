500b43d9d88022824e40e2ce8727cb510949d765
MichielClever <michiel@clever.be>
2018-02-12 11:08:50 +0100

    new lesson

--- notes-node/playground/arrow-function.js
+++ notes-node/playground/arrow-function.js
@@ -1,7 +1,18 @@
-var square = (x)=>{
-  var result= x*x;
-    return result
+var square = (x)=> x*x;
+console.log(square(9));
+
+var user = {
     
+    name: "Michiel",
+    sayHi: () => {;
+        console.log(arguments);
+        console.log(`Hi. I'm ${this.name}`)
+    },
+    
+    sayHiAlt() {
+        console.log(arguments);
+        console.log(`Hi. I'm ${this.name}`);
+    }
 };
 
-console.log(square(9));
+user.sayHiAlt(1,2,3);
